b1e95fbb500c2330da8489bb454e249a7d7df617
AANG-804 <bonjour8084@naver.com>
2023-10-15 20:47:30 +0900

    Prompt revision

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import openai
 import streamlit as st
 import os  # Importing the OS library to interact with the operating system
 from dotenv import load_dotenv  # Importing the function to load .env variables
+from modules import RAG
 
 
 def init():
     load_dotenv()
-
     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
         print("OPENAI_API_KEY is not set")
         exit(1)
     else:
         print("API Key is set!")
+        openai_api_key = os.getenv("OPENAI_API_KEY")
+        openai.api_key = openai_api_key  # Set the OpenAI API key
 
     st.set_page_config(
         page_title="Firelit Chat AI",
@@ -26,20 +28,24 @@ def init():
 
 
 def main():
+
     init()
     # Initializing a message list in the session state if it's not present
     if "messages" not in st.session_state:
         st.session_state.messages = [
-            # content에 시작 메세지를 담을 수 있음
-            {
+            {  # 프롬프트 엔지니어링 하는 부분
                 "role": "system",
                 "content":
                     "You are an expert of a market analysis in restaurant field. You'll be able to communicate accurate,\
                     informed analysis in an easy-to-understand manner to people who want to start a business in the catering sector.\
                     You must be straightforward and responsive to customers, and avoid using ambiguous language.\
-                    Answers should include appropriate comparision with other district.",
+                    Answers should include appropriate comparision with other district.\
+                    and also you should follow the rules bellow\
+                    **important rules \
+                    1/ Answer the user's request within 200 tokens unconditionally\
+                    2/ If you don't have enough tokens for the response, ask the user to enter '계속하기'",
             },
-            {"role": "assistant", "content": "어떻게 도와드릴까요?"}]
+            {"role": "assistant", "content": "창업을 하기 위해서는 상권분석, 입지분석, 점포분석의 과정으로 이루어져있어요. 어떻게 도와드릴까요?"}]  # 유저에게 가장 먼저 던지는 메세지
 
     # Displaying the list of messages on the main page
     for msg in st.session_state.messages[1:]:
@@ -50,7 +56,7 @@ def main():
         # Displaying the user's message on the main page
         st.chat_message("user").write(prompt)
 
-        # prompt 처리하기 <- 우리가 원하는 형태를 정제할 수 있음 RAG를 하는 부분
+        # 유저 입력, prompt 처리하기 <- 우리가 원하는 형태를 정제할 수 있음 RAG를 하는 부분
         pass
 
         # Adding the user's message to the list of messages
@@ -59,7 +65,7 @@ def main():
         # Making a request to OpenAI's API to get a response based on the list of messages
         with st.spinner("응답 생성중..."):
             response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo", messages=st.session_state.messages)
+                model="gpt-3.5-turbo-0613", messages=st.session_state.messages, max_tokens=400, temperature=0)
 
         # Extracting the assistant's response from the API response
         msg = response.choices[0].message
